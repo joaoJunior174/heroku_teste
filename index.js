876300ab6da9b61a6bf73df23b21816eb30e5592
Joao aurefelio francisco junior <jafrancisco@rd.com.br>
2021-10-27 20:37:05 -0300

    server

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('<h1>hello world!</h1>'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
