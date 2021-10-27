d878afe4d44a5ccd9c13a486b40e70e7917ba6ca
Joao aurefelio francisco junior <jafrancisco@rd.com.br>
2021-10-27 20:04:59 -0300

    server

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var porta = process.env.PORT || 8080;
+app.listen(porta);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
